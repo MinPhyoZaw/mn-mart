07f2dcb13e7d45fa6b72eb7cd72ca5cd51722142
minphyo_thaw <42090584+MinPhyoZaw@users.noreply.github.com>
2026-04-03 14:36:44 +0630

    Add category-specific shop pages and category filtering

---
+++ app/spa/page.js
@@ -0,0 +1,12 @@
+import CategoryShopsPage from "../components/CategoryShopsPage";
+
+export default function SpaPage() {
+  return (
+    <CategoryShopsPage
+      category="spa"
+      title="Spa"
+      heroImage="/images/nail-spa.jpg"
+      ctaLabel="Book Now"
+    />
+  );
+}
